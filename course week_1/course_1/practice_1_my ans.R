7a8c5fd60212b92cdb72c5b58a57ed38947414c1
cafepeng <20900139+cafepeng@users.noreply.github.com>
2018-03-01 12:01:40 +0800

    course

---
+++ course week_1/course_1/practice_1_my ans.R
@@ -0,0 +1,28 @@
+### practice_1_answer
+### BMI Caculator
+
+# Craete variable called my.height.cm with your actual height in cm 
+my.height.cm <-160
+
+# Craete variable called my.weight.cm with your actual weight in kg
+my.weight.kg <-55
+
+# Create my.height.m transfered by my.height.cm  
+my.height.m <- 160/100
+
+# Create my.bmi with BMI(Body Mass Index) formula
+my.bmi <- my.weight.kg/my.height.m^2
+
+# Use if-else to print matched information
+# Reference: http://www.tpech.gov.taipei/ct.asp?xItem=1794336&CtNode=30678&mp=109171
+if (my.bmi >= 35) {
+  print(paste("Your bmi: ", my.bmi))
+  print("重度肥胖!")
+} else if (my.bmi==30){
+  print(paste("Your bmi: ", my.bmi))##print two results##
+  print("適中!")
+} else {
+  print(paste("Your bmi:",my.bmi))
+  print("Very GOOD!!")
+}
+
