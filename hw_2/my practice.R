40672834afb5ece0f81d09131b0dbd30f8cbe27e
cafepeng <20900139+cafepeng@users.noreply.github.com>
2018-03-13 10:26:07 +0800

    update130318

---
+++ hw_2/my practice.R
@@ -0,0 +1,13 @@
+
+##----------------------------------##
+library(rvest)
+#輸入網址，爬取此網頁
+title<-read_html("http://news.ltn.com.tw/list/newspaper/world/20180313")
+#標題內含css
+title<-html_nodes(title,".tit p")
+title<-html_text(title)
+#title<-html_nodes(title,"UTF-8")
+
+#若文字出現亂碼，將格式改為可辨識的形式
+title<-iconv(title,"UTF-8")
+title
